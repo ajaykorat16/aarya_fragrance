cb888505246f5377f5780eee524d22831cd3a22f
Ajay Korat <ajaykorat16@gmail.com>
2023-09-29 18:49:22 +0530

    product image frontend pagination

--- src/Controller/FrontEndProductController.php
+++ src/Controller/FrontEndProductController.php
@@ -7,17 +7,26 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Repository\ImageRepository;
 use App\Repository\ProductRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FrontEndProductController extends AbstractController
 {
     #[Route('/', name: 'app_product_display')]
-    public function display(ProductRepository $productRepository,ImageRepository $imageRepository): Response
+    public function display(ProductRepository $productRepository,ImageRepository $imageRepository,Request $request,PaginatorInterface $paginator): Response
     {
+        $pagination = $paginator->paginate(
+            $imageRepository->findAll(),
+            $request->get('page', 1),
+            $this->getParameter('page_limit')
+        );
+
         return $this->render('product/display.html.twig',[
             'products' =>$productRepository->findAll(),
+            'pagination' => $pagination,
             'images' =>$imageRepository->findAll(),
         ]);
     }
